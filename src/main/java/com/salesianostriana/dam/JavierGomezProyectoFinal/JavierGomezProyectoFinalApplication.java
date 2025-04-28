070a3d0b5eb457220d087a89b5df5c4caaa0c70e
Javier Gomez <gomezjavi2004@gamil.com>
2025-04-28 11:51:36 +0200

    primer commit del proyecto

---
+++ src/main/java/com/salesianostriana/dam/JavierGomezProyectoFinal/JavierGomezProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.JavierGomezProyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavierGomezProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavierGomezProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/JavierGomezProyectoFinal/JavierGomezProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.JavierGomezProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavierGomezProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
